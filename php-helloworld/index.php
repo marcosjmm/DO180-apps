519e4ab0cfa99c223c64b0866bfff6707a72b24a
Student User <student@workstation.lab.example.com>
2023-03-22 10:53:02 -0400

    faltou o ;

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Uma alteracao feita por Marcos Melo está no ar!"\n
+print "Uma alteracao feita por Marcos Melo está no ar!\n";
 ?>
